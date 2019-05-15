b6a3311e202e22d2ed61b8a8dd8ba9328f998a9b
Yuriuh <642192014@qq.com>
2019-05-15 12:16:51 +0800

    更新 package.json
    更新 package.json

---
+++ index.js
@@ -0,0 +1,27 @@
+import Button from './src/components/Button'
+import ButtonGroup from './src/components/Button-Group'
+import Icon from './src/components/Icon'
+import Input from './src/components/Input'
+import Popover from './src/components/Popover'
+import Toast from './src/components/Toast'
+import plugin from './src/components/plugin'
+import Tabs from './src/components/tabs/Tabs'
+import TabsHead from './src/components/tabs/Tabs-Head'
+import TabsBody from './src/components/tabs/Tabs-Body'
+import TabsItem from './src/components/tabs/Tabs-Item'
+import TabsPane from './src/components/tabs/Tabs-Pane'
+
+export {
+  Button,
+  ButtonGroup,
+  Icon,
+  Input,
+  Popover,
+  Toast,
+  plugin,
+  Tabs,
+  TabsHead,
+  TabsBody,
+  TabsItem,
+  TabsPane,
+}
